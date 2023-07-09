8f26c1d58172e6b15ab5cb3ed22331421b2aa9ec
naveen5412 <86046284+naveen5412@users.noreply.github.com>
2023-07-09 22:36:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,246 @@
+import gsap from 'https://cdn.skypack.dev/gsap';
+import ScrollTrigger from 'https://cdn.skypack.dev/gsap/ScrollTrigger';
+import Draggable from 'https://cdn.skypack.dev/gsap/Draggable';
+
+gsap.registerPlugin(ScrollTrigger, Draggable);
+
+gsap.set('.box', {
+  yPercent: -50 });
+
+
+const STAGGER = 0.1;
+const DURATION = 1;
+const OFFSET = 0;
+const BOXES = gsap.utils.toArray('.box');
+
+const LOOP = gsap.timeline({
+  paused: true,
+  repeat: -1,
+  ease: 'none' });
+
+
+const SHIFTS = [...BOXES, ...BOXES, ...BOXES];
+
+SHIFTS.forEach((BOX, index) => {
+  const BOX_TL = gsap.
+  timeline().
+  set(BOX, {
+    xPercent: 250,
+    rotateY: -50,
+    opacity: 0,
+    scale: 0.5 })
+
+  // Opacity && Scale
+  .to(
+  BOX,
+  {
+    opacity: 1,
+    scale: 1,
+    duration: 0.1 },
+
+  0).
+
+  to(
+  BOX,
+  {
+    opacity: 0,
+    scale: 0.5,
+    duration: 0.1 },
+
+  0.9)
+
+  // Panning
+  .fromTo(
+  BOX,
+  {
+    xPercent: 250 },
+
+  {
+    xPercent: -350,
+    duration: 1,
+    immediateRender: false,
+    ease: 'power1.inOut' },
+
+  0)
+
+  // Rotations
+  .fromTo(
+  BOX,
+  {
+    rotateY: -50 },
+
+  {
+    rotateY: 50,
+    immediateRender: false,
+    duration: 1,
+    ease: 'power4.inOut' },
+
+  0)
+
+  // Scale && Z
+  .to(
+  BOX,
+  {
+    z: 100,
+    scale: 1.25,
+    duration: 0.1,
+    repeat: 1,
+    yoyo: true },
+
+  0.4).
+
+  fromTo(
+  BOX,
+  {
+    zIndex: 1 },
+
+  {
+    zIndex: BOXES.length,
+    repeat: 1,
+    yoyo: true,
+    ease: 'none',
+    duration: 0.5,
+    immediateRender: false },
+
+  0);
+
+  LOOP.add(BOX_TL, index * STAGGER);
+});
+
+const CYCLE_DURATION = STAGGER * BOXES.length;
+const START_TIME = CYCLE_DURATION + DURATION * 0.5 + OFFSET;
+
+const LOOP_HEAD = gsap.fromTo(
+LOOP,
+{
+  totalTime: START_TIME },
+
+{
+  totalTime: `+=${CYCLE_DURATION}`,
+  duration: 1,
+  ease: 'none',
+  repeat: -1,
+  paused: true });
+
+
+
+const PLAYHEAD = {
+  position: 0 };
+
+
+const POSITION_WRAP = gsap.utils.wrap(0, LOOP_HEAD.duration());
+
+const SCRUB = gsap.to(PLAYHEAD, {
+  position: 0,
+  onUpdate: () => {
+    LOOP_HEAD.totalTime(POSITION_WRAP(PLAYHEAD.position));
+  },
+  paused: true,
+  duration: 0.25,
+  ease: 'power3' });
+
+
+let iteration = 0;
+const TRIGGER = ScrollTrigger.create({
+  start: 0,
+  end: '+=2000',
+  horizontal: false,
+  pin: '.boxes',
+  onUpdate: self => {
+    const SCROLL = self.scroll();
+    if (SCROLL > self.end - 1) {
+      // Go forwards in time
+      WRAP(1, 1);
+    } else if (SCROLL < 1 && self.direction < 0) {
+      // Go backwards in time
+      WRAP(-1, self.end - 1);
+    } else {
+      const NEW_POS = (iteration + self.progress) * LOOP_HEAD.duration();
+      SCRUB.vars.position = NEW_POS;
+      SCRUB.invalidate().restart();
+    }
+  } });
+
+
+const WRAP = (iterationDelta, scrollTo) => {
+  iteration += iterationDelta;
+  TRIGGER.scroll(scrollTo);
+  TRIGGER.update();
+};
+
+const SNAP = gsap.utils.snap(STAGGER);
+
+const progressToScroll = (progress) =>
+gsap.utils.clamp(
+1,
+TRIGGER.end - 1,
+gsap.utils.wrap(0, 1, progress) * TRIGGER.end);
+
+
+const scrollToPosition = position => {
+  const SNAP_POS = SNAP(position);
+  const PROGRESS =
+  (SNAP_POS - LOOP_HEAD.duration() * iteration) / LOOP_HEAD.duration();
+  const SCROLL = progressToScroll(PROGRESS);
+  if (PROGRESS >= 1 || PROGRESS < 0) return WRAP(Math.floor(PROGRESS), SCROLL);
+  TRIGGER.scroll(SCROLL);
+};
+
+ScrollTrigger.addEventListener('scrollEnd', () =>
+scrollToPosition(SCRUB.vars.position));
+
+
+const NEXT = () => scrollToPosition(SCRUB.vars.position - 1 / BOXES.length);
+const PREV = () => scrollToPosition(SCRUB.vars.position + 1 / BOXES.length);
+
+document.addEventListener('keydown', event => {
+  if (event.code === 'ArrowLeft' || event.code === 'KeyA') NEXT();
+  if (event.code === 'ArrowRight' || event.code === 'KeyD') PREV();
+});
+
+document.querySelector('.boxes').addEventListener('click', e => {
+  const BOX = e.target.closest('.box');
+  if (BOX) {
+    // Get the current index
+    const IDX = gsap.utils.wrap(0, 10, Math.floor(SCRUB.vars.position * 10));
+    const TARGET = BOXES.indexOf(BOX);
+    // Can only hit three values
+    // -0.2, -0.1, 0, 0.1, 0.2
+    // Base it on the current index and then the positions around it.
+    // In most cases the bump will be TARGET - IDX
+    let bump = TARGET - IDX;
+    // If we're in the top half and need to wrap around
+    if (IDX >= BOXES.length - 2 && TARGET < IDX && TARGET < 2) bump = 2;
+    if (IDX >= BOXES.length - 1 && TARGET === 0) bump = 1;
+    // If we're in the lowers and need to wrap back
+    if (IDX <= 2 && TARGET > IDX && TARGET >= BOXES.length - 2) bump = -2;
+    if (IDX < 1 && TARGET === BOXES.length - 1) bump = -1;
+    if (Math.abs(bump) <= 2)
+    scrollToPosition(SCRUB.vars.position + 1 / BOXES.length * bump);
+  }
+});
+
+window.BOXES = BOXES;
+
+document.querySelector('.next').addEventListener('click', NEXT);
+document.querySelector('.prev').addEventListener('click', PREV);
+
+// Dragging
+Draggable.create(".drag-proxy", {
+  type: "x",
+  trigger: ".boxes",
+  onPress() {
+    this.startPosition = SCRUB.vars.position;
+  },
+  onDrag() {
+    SCRUB.vars.position = this.startPosition + (this.startX - this.x) * 0.001;
+    SCRUB.invalidate().restart(); // same thing as we do in the ScrollTrigger's onUpdate
+  },
+  onDragEnd() {
+    scrollToPosition(SCRUB.vars.position);
+  } });
+
+
+gsap.set('.box', { display: 'block' });
+
+setInterval(NEXT, 4000);
